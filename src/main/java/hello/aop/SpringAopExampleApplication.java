12ba9a8fdf55a8857195b951fcb1f2a917a0e15a
Seong-Gook Lim <sklim0932@gmail.com>
2022-02-14 22:29:31 +0900

    init commit

---
+++ src/main/java/hello/aop/SpringAopExampleApplication.java
@@ -0,0 +1,13 @@
+package hello.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopExampleApplication.class, args);
+	}
+
+}
